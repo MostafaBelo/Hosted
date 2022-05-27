ba365281be6735f2da0c076b07556bc45817f322
MostafaElfaggal <mostafaelfaggal@aucegypt.edu>
2022-05-27 11:29:35 +0200

    git init

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+const port = 3000;
+
+app.get('/home', (req, res) => {
+    console.log(req.ip);
+    console.log('lol');
+    res.send({'msg':'lol this is working somehow!'});
+});
+
+app.listen(port, () => {
+    console.log(`Running at http://localhost:${port}`);
+});
