7e01729ed038337bed439799b86547f9f95b305a
MostafaElfaggal <mostafaelfaggal@aucegypt.edu>
2022-05-27 11:59:57 +0200

    port updated to 8080

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require("express");
 
 const app = express();
-const port = 3000;
+const port = 8080;
 
-app.get('/home', (req, res) => {
+app.get("/home", (req, res) => {
     console.log(req.ip);
-    console.log('lol');
-    res.send({'msg':'lol this is working somehow!'});
+    console.log("lol");
+    res.send({ msg: "lol this is working somehow!" });
 });
 
 app.listen(port, () => {
